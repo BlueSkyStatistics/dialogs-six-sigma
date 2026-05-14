a06ad230a681fd2f929de6c7d08b0d9cc67435f1
Aspect13 <ad13box@gmail.com>
2026-05-14 22:05:00 +0200

    Refactor navigation structure by replacing names with IDs for menu items and cleaning up commented code

--- nav.js
+++ nav.js
@@ -1,20 +1,11 @@
-/**
-  * This file is protected by copyright (c) 2023-2025 by BlueSky Statistics, LLC.
-  * All rights reserved. The copy, modification, or distribution of this file is not
-  * allowed without the prior written permission from BlueSky Statistics, LLC.
- */
-
-const {getT} = global.requireFromRoot("localization");
-let t = getT('menutoolbar')
-const nav = () => ({
-    "name": t('sixsigma_top_level_title'),// {ns: 'menutoolbar'}),
-    "tab": "six_sigma",
+const nav = {
+    "id": "menu-sixsigma",
     "buttons": [
         "./SixSigmaOverview",
         "./createCauseEffectDiagram",
         "./lossFunctionAnalysis",
         {
-            "name": t('sixsigma_MSA'),// {ns: 'menutoolbar'}),
+            "id": "menu-sixsigma-msa",
             "icon": "icon-sixsigma",
             "children": [
                 "./gageRnR",
@@ -23,37 +14,34 @@ const nav = () => ({
             ]
         },
         {
-            "name": t('sixsigma_Process_Capability'),// {ns: 'menutoolbar'}),
+            "id": "menu-sixsigma-processcapability",
             "icon": "icon-sixsigma",
             "children": [
-				"./processCapabilityQcc"
+ 				"./processCapabilityQcc"
             ]
         },
         {
-            "name": t('sixsigma_Shewhart_Charts'),// {ns: 'menutoolbar'}),
+            "id": "menu-sixsigma-shewhartcharts",
             "icon": "icon-sixsigma",
             "children": [
                 "./shewhartCharts1",
                 "./shewhartCharts2",
                 "./shewhartCharts3",
                 "./shewhartCharts4"
-				
+
             ]
         },
 	    {
-			"name": t('sixsigmapro_Reliability'),
+			"id": "menu-sixsigma-reliability-pro",
 			"icon": "icon-sixsigma",
 			"children": [
-			
+
 			]
-		},			
-        "./cusumChart",	
+		},
+        "./cusumChart",
         "./ewmaChart",
         "./mqccChart"
     ]
-})
-
-module.exports = {
-    nav: nav(),
-    render: () => nav()
 }
+
+module.exports.nav = nav
